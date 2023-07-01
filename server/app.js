cee4fea02e719e1c099e4bb3415bd6f85a50f935
ThaiIT <thaibmt47@gmail.com>
2023-07-02 00:29:49 +0700

    set cors to backend

--- server/app.js
+++ server/app.js
@@ -4,9 +4,11 @@ const express = require("express");
 const MapPackage = require("./components/MapPackage");
 const { callLayer } = require("./components/connectDb");
 const connectDb = require("./components/connectDb");
-
+const cors = require('cors');
 const app = express();
-
+app.use(cors({
+  origin: '*'
+}));
 // app.use('/', express.static(path.join(__dirname, '../dist')));
 app.get("/api/vector/:layerId/:z/:x/:y", async (req, res) => {
   try {
